6555e7d548bb38e8362ac5c4dbd45a96ab2842e8
proton5 <jessehillman7@hotmail.com>
2022-10-03 19:21:50 -0600

    added Magic Eight Ball script

---
+++ magicEightBall.py
@@ -0,0 +1,3 @@
+import random
+messages = ['It is certain','It is decidedly so','Yes definetely','Reply hazy try again','ask again later','Concentrate and ask again','My reply is no','Outlook not so good','Very doubtful']
+print(messages[random.randint(0, len(messages) -1)])
